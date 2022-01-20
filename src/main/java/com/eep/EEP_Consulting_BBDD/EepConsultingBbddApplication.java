089f391b3aa4e9bd477f3498f37429e99ffcd07f
roboc <ccastillo@eepmad.com>
2022-01-20 11:50:31 +0100

    Inicio de app EEP_Consulting_BBDD

---
+++ src/main/java/com/eep/EEP_Consulting_BBDD/EepConsultingBbddApplication.java
@@ -0,0 +1,13 @@
+package com.eep.EEP_Consulting_BBDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EepConsultingBbddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EepConsultingBbddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eep/EEP_Consulting_BBDD/EepConsultingBbddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eep.EEP_Consulting_BBDD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EepConsultingBbddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
